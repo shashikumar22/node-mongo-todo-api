6a270aa8e313cc4f89da96e8d9946a4aab5b47c4
shashi <shashikumar2203@gmail.com>
2018-03-03 23:03:57 +0530

    postman post request

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+	console.log(req.body)
+	var newTodo = new Todo({
+		text: req.body.text
+	});
+
+	newTodo.save().then((docs) => {
+		res.send(docs);
+	}, (e) => {
+		res.status(400).send(e);
+	})
+});
+
+app.listen(3000, () => {
+	console.log("connecting to port 3000")
+});
