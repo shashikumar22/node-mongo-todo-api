dadcca8919ce2954cee86466cb14a16552e5230c
shashi <shashikumar2203@gmail.com>
2018-03-05 00:08:59 +0530

    get by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 var app = express();
 
 var {mongoose} = require('./db/mongoose');
@@ -28,6 +29,22 @@ app.get('/todos',(req, res) => {
 		res.status(400).send(e);
 	})
 });
+
+app.get('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo){
+			res.status(404).send();			
+		}
+		res.status(200).send({todo});
+	}).catch((e) =>{
+		res.status(400).send();
+	})
+})
 
 app.listen(3000, () => {
 	console.log("connecting to port 3000")
