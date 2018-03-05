f969bb7800bbdd28ab5484b5552730d62be603b9
shashi <shashikumar2203@gmail.com>
2018-03-05 17:12:49 +0530

    update todos

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,14 +1,38 @@
-const {ObjectId} = require('mongodb');
-const {mongoose} = require('../server/db/mongoose');
-const {Todo} = require('../server/models/todo');
-const {User} = require('../server/models/user');
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
-var id = "3a9c2f319c17400c3c2e2f6b";
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-if(!ObjectId.isValid(id)){
-  console.log('Id not valid');
-}
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('57bc4b15b3b6a3801d8c47a2')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
 
-Todo.remove({}).then((res) => {
-	console.log(res);
-})
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('57abbcf4fd13a094e481cf2c')
+  }, {
+    $set: {
+      name: 'Andrew'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 var app = express();
 
 var {mongoose} = require('./db/mongoose');
@@ -46,6 +47,32 @@ app.get('/todos/:id', (req,res) => {
 		res.status(400).send();
 	})
 });
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	} else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set:body}, {new:true}).then((todo) =>{
+		if(!todo){
+			res.status(404).send();
+		}
+
+		res.send({todo})
+	}).catch((e) => {
+		res.status(400).send();		
+	})
+})
 
 app.delete('/todos/:id', (req,res) => {
 	var id = req.params.id;
